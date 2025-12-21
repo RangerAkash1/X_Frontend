4075b04a11c3f779666f7a44b28fb69c78951ae7
RangerAkash1 <akash13032002@gmail.com>
2025-12-21 22:28:13 +0530

    routing and response model

---
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,12 @@
+//dashboard-routing module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardHome } from './components/dashboard-home/dashboard-home'; 
+const routes: Routes = [
+    { path: '', component: DashboardHome }
+];
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
