b608845951021cd3479edacde2d217bd9333ea4a
RangerAkash1 <akash13032002@gmail.com>
2025-12-29 23:54:57 +0530

    extra model

--- src/app/shared/models/reponse.model.ts
+++ src/app/shared/models/reponse.model.ts
@@ -3,3 +3,18 @@ export interface ResponseModel<T> {
     message: string;
     data: T;
 }
+
+export interface PaginatedResponseModel<T> {
+    success: boolean;
+    message: string;
+    data: T[];
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+}
+
+export interface ErrorResponseModel {
+    success: boolean;
+    message: string;
+    errorCode: number;
+}   
